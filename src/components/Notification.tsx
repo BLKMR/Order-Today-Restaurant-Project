918ad33736b6e1c6db8aace3166eed4c2e51ef29
Michael Blackmer <michael.blackmer@na.games-workshop.net>
2023-08-10 16:46:46 -0500

    update

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Free deliver for all orders over $50! Click here to order today!"</div>
+  )
+}
+
+export default Notification
