f73242507ed3f7189a85cac158cf4c1edff9a47b
Michael Blackmer <michael.blackmer@na.games-workshop.net>
2023-08-14 16:21:51 -0500

    Slider component built

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import FeaturedItems from '@/components/FeaturedItems'
+import Offer from '@/components/Offer'
+import Slider from '@/components/Slider'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>HomePage
+      <Slider />
+      <FeaturedItems />
+      <Offer />
+    </div>
   )
 }
 
