a4999cc41b23e28f76a73de00690f7834fcfe13c
Michael Blackmer <michael.blackmer@na.games-workshop.net>
2023-08-17 16:50:23 -0500

    Offer componentcreated and styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage
+    <div>
       <Slider />
       <Featured />
       <Offer />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center">
-      <Link href="/">BARDOG TAVERN</Link>
-      <p>ALL RIGHTS RESERVED</p>    
+    <div className="h-12 md:h24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center">
+      <Link href="/"  className="font-bold text-xl">BARDOG TAVERN</Link>
+      <p>@ALL RIGHTS RESERVED</p>    
     </div>
   )
 }
